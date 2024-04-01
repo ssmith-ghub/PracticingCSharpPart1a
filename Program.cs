4e4a6e9c828015075f640c2e79c6170f40051976
Stephen Smith <ssmith_138823@outlook.com>
2024-03-31 21:37:19 -0400

    pushing repo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm practicing printing a string of code!");
+            Console.WriteLine("I'm also practicing typing out the main program code!");
+            Console.WriteLine("hoping repetition will help me!");
+            Console.Beep();
+        }
+    }
+}
